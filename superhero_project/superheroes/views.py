b94b0f4a7cddda40dc5c208832dec3980c261cdb
andrew morales <andrew.morales725@gmail.com>
2021-10-29 16:07:26 -0400

    Added edit view linked to index

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -34,6 +34,28 @@ def create(request):
     else:
         return render(request, 'superheroes/create.html')
 
+def edit(request, hero_id):
+    single_hero = Superhero.objects.get(pk = hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+
+        single_hero.name = name
+        single_hero.alter_ego = alter_ego
+        single_hero.primary_ability = primary
+        single_hero.secondary_ability = secondary
+        single_hero.catch_phrase = catchphrase
+        single_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/edit.html', context)
+
 def delete(request, hero_id):
     single_hero = Superhero.objects.get(pk = hero_id)
     single_hero.delete()
